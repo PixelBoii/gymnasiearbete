f9579217a7f4c0c44e48a16dc9513454f7c81863
PixelBoii <priwer.kontakt@gmail.com>
2022-03-14 10:49:57 +0100

    refactor: Redirect to login page instead of displaying welcome page

--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,7 @@ use App\Http\Controllers\DeviceController;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return redirect('/login');
 });
 
 Route::prefix('dashboard')->middleware(['auth', 'verified'])->group(function() {
